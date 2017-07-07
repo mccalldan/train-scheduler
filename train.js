9571786b1802c11634863c9ccbd4f8f2c6dc368a
mccalldan <drm8266@gmail.com>
2017-07-06 20:47:01 -0400

    add

--- train.js
+++ train.js
@@ -37,10 +37,10 @@ $("#submit-btn").on("click", function(event) {
    alert("Train successfully added");
 
   // Clears all of the text-boxes
-  $("#employee-name-input").val("");
-  $("#role-input").val("");
-  $("#start-input").val("");
-  $("#rate-input").val("");
+  $("#train-name").val("");
+  $("#destination").val("");
+  $("#time").val("");
+  $("#frequency").val("");
 });
 
 // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
@@ -54,21 +54,41 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   var trainTime = childSnapshot.val().time;
   var trainFrequency = childSnapshot.val().frequency;
 
-  // Employee Info
+  //  Consol
   console.log(trainName);
   console.log(trainDestination);
   console.log(trainTime);
   console.log(trainFrequency);
 
-  // Prettify the employee start
-  var trainTimeClean = moment.unix(trainTime).format("HH:MM");
+ 
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(trainTime, "hh:mm A");
+    console.log(firstTimeConverted);
 
-  // Calculate the minutes in frequency
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = trainFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
   // Add each train's data into the table
   $("#train-table").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
-  trainTimeClean + "</td><td>" + trainFrequency + "</td>")
+  trainTime + "</td><td>" + nextTrain.format("hh:mm A") + "</td><td>" + trainFrequency + "</td>");
 
 });
 
